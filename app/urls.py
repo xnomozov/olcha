6092e49540e6aa9c88d899e4f71940030bb01cb7
xakimjon <xakimjonnomozov8@gmail.com>
2024-08-06 18:10:31 +0500

    written api for crud categories

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,9 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('categories/', views.CategoryList.as_view(), name='category-list'),
+    path('categories/', views.CategoryListView.as_view(), name='category-list'),
+    path('category/<slug:slug>/', views.CategoryDetail.as_view(), name='category-detail'),
+    path('category-create/', views.CreateCategoryView.as_view(), name='category-create'),
+    path('category/<slug:slug>/update/', views.UpdateCategoryView.as_view(), name='category-update'),
+    path('category/<slug:slug>/delete/', views.DeleteCategoryView.as_view(), name='category-delete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,81 @@
 # Create your views here.
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import generics, status
 from app.models import Category
+from app.serializers import CategorySerializer
 
 
-class CategoryList(APIView):
-    def get(self, request, format=None):
-        data = [{'title': category.title,
-                 'image': request.build_absolute_uri(category.image.url)
-                 } for category in Category.objects.all()]
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-        return Response(data)
+
+class CategoryDetail(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = Category.objects.get(slug=slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = Category.objects.get(slug=slug)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = Category.objects.get(slug=slug)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CreateCategoryView(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class UpdateCategoryView(generics.UpdateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = get_object_or_404(Category, slug=slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = get_object_or_404(Category, slug=slug)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteCategoryView(generics.DestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = get_object_or_404(Category, slug=slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = get_object_or_404(Category, slug=slug)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
